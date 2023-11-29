0a41ff0106c6bfe056c8af552a9bfea73597b98f
MadLad <shivam.119966@yahoo.com>
2023-11-29 21:39:54 +0530

    removed manifest

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import {
+  DynamicContextProvider,
+} from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import './App.css';
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:'flex', justifyContent:'center',alignItems:'center', height:'100vh'}}>
+      <DynamicContextProvider
+        settings={{
+          environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
+          walletConnectors: [
+            EthereumWalletConnectors,
+          ],
+        }}
+      >
+        <Main />
+      </DynamicContextProvider>
     </div>
   );
 }
